58c900bdbd3aa99378a94390349a9506cb2c2db5
Chris <sr.highyitan@gmail.com>
2020-11-03 06:54:15 +0100

    Multi and sum

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+#Example
 # def print_hi(name):
 #     print(f'Hi, {name}')
 #     if __name__ == '__main__':
 #         print_hi('PyCharm')
-def hello():
-    print(f'Hello world')
+#Hello
+# def hello():
+#     print(f'Hello world')
+#
+#
+# hello()
+n1 = int(input("Number 1"))
+n2 = int(input("Number 2"))
+def product(n1,n2):
+    result = n1 * n2
+    if result > 1000:
+        result = n1 + n2
+        return result
+    else:
+        return result
 
-
-hello()
+result = product(n1,n2)
+print("\nResult is", result)
