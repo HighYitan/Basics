18b56ba170aaa4a5fb4dee2e4c8c89c1b93e5b7d
Chris <sr.highyitan@gmail.com>
2020-11-03 09:11:24 +0100

    Multi and sum

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Example
+#Example of Pycharm
 # def print_hi(name):
 #     print(f'Hi, {name}')
 #     if __name__ == '__main__':
@@ -9,15 +9,26 @@
 #
 #
 # hello()
-n1 = int(input("Number 1"))
-n2 = int(input("Number 2"))
-def product(n1,n2):
-    result = n1 * n2
-    if result > 1000:
-        result = n1 + n2
-        return result
-    else:
-        return result
-
-result = product(n1,n2)
-print("\nResult is", result)
+#Multi and sum
+# n1 = int(input("Number 1 "))
+# n2 = int(input("Number 2 "))
+# def product(n1,n2):
+#     result = n1 * n2
+#     if result > 1000:
+#         result = n1 + n2
+#         return result
+#     else:
+#         return result
+#
+# result = product(n1,n2)
+# print("\nResult is", result)
+#Range iteration
+def Ranged(number):
+    lastnumber = 0
+    for iteration in range(number):
+        sum = lastnumber + iteration
+        print(f"Our number,", iteration, "Last number,", lastnumber, "Sum", sum)
+        lastnumber = iteration
+n = 10
+print("Printing until", n,"the number and lastnumber and their sum \n")
+Ranged(n)
